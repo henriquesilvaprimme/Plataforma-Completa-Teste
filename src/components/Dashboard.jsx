7d51a4d0d6c7505faf1a9e117f538528d588a0d7
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-11-27 17:42:29 -0300

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { collection, onSnapshot } from 'firebase/firestore';
-import { db } from './firebase';
+import { db } from '../firebase';
 import { Users, DollarSign, PhoneCall, PhoneOff, Calendar, XCircle, TrendingUp } from 'lucide-react';
 
 const Dashboard = ({ usuarioLogado }) => {
