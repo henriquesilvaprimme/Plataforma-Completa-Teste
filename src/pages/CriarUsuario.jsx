c663ac134cbddd34cd42703fabd250f3e0bd1845
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-11-27 02:00:32 -0300

    Update CriarUsuario.jsx

--- src/pages/CriarUsuario.jsx
+++ src/pages/CriarUsuario.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { db } from './firebase';
+import { db } from '../firebase';
 import { doc, setDoc } from 'firebase/firestore';
 
 const CriarUsuario = ({ adicionarUsuario }) => {
